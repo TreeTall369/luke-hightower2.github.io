ad4c9287d5a10da99e8bf2cd933d64cda7602ef9
Luke Hightower <lukehightower11@gmail.com>
2022-12-16 21:12:31 +0000

    dec16

--- projects/eloquent-javascript/chapter-04/exercises.js
+++ projects/eloquent-javascript/chapter-04/exercises.js
@@ -2,7 +2,7 @@
 // range ///////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function range(num1, num2) {
+function range(num1, num2, step) {
 // fx should create array with contents of integers within given range
 // need loop for counting up if num1 < is lesser than num2 
 // need looop for counting down num1 > is greater than num2
@@ -31,13 +31,13 @@ if (step === undefined){
     return array
     } else if (num1 < num2){
     // for loop count up from num1 to num2 BY THE STEP FACTOR
-    for (let i = num1; i <= num2; i + step){
+    for (let i = num1; i <= num2; i += step){
       // push ascending range by step into array 
       array.push(i)
     }
     } else {
     // for loop count down from num1 to num2 BY THE STEP FACTOR
-    for (let j = num1; j >= num2; j - step){
+    for (let j = num1; j >= num2; j -= step){
       // push descending range by step into array
       array.push(j)
     }
@@ -96,29 +96,55 @@ function arrayToList(array) {
   return list;
 }
 
-
 ////////////////////////////////////////////////////////////////////////////////
 // listToArray /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-
-function listToArray(list) {
-  
+                          // must use default parameter, going to return in recursion
+function listToArray(list, array=[]) {
+// base case
+// recrusion will stop when list.rest reaches null
+if (list.rest === null){
+// on last step of recursion we need to add final value from the last object
+  array.push(list.value)
+// after that recursion is ending so we return the array, now completed having hit the base case
+  return array
+}
+// here we push the desired value into the array, using dot notion to grab the value out of each object
+array.push(list.value)
+// recursive case
+// we need to return list.rest ie the other key in the object, that value is the next layer of the nested object. 
+// this with go to the base case, if not null, it will push list.value, as programmed. 
+ return listToArray(list.rest, array)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // prepend /////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function prepend() {
-
+function prepend(parameter, list){
+  // inserts value into list at first position (before all other elements)
+  // declare variable set to function expressed. will be array
+  let arrayInfo = listToArray(list, array=[]); 
+  // unshift desried parameter into array 
+  arrayInfo.unshift(parameter)
+  // set variable equal to function arraToList, will be list (object)
+  let newList = arrayToList(arrayInfo)
+  // return new list with parameter added properly
+      return newList
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // nth /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function nth() {
-
+function nth(list, index) {
+// tkes list (object) index given as number
+// declare variable equal to arry from listtoarray function
+let arrayInfo = listToArray(list, array=[]);
+// access desired info with bracket notice b/c we only know the index.
+let value = arrayInfo[index]
+//return the element
+return value;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
